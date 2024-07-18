0ff96df6ceb063bcbe6f553c2055be8ae73168b8
sintacamelia <sintacamelia32@gmail.com>
2024-07-18 11:28:05 +0700

    Add files via upload

---
+++ angkot3.js
@@ -0,0 +1,14 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6; 
+var noAngkot = 1;
+
+for( var noAngkot =1; noAngkot <=jmlAngkot; noAngkot++) {    
+    
+ if(noAngkot <= 6 ){
+    console.log('Angkot no. ' + noAngkot + 'beroperasi dengan baik');
+}else{
+    console.log('Angkot no. ' + noAngkot + 'sedang tidak beroperasi')
+}
+}
+      
+  
